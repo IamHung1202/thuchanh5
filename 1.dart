71d61fdfd0959d9657c0ba9ad017208dcd1bad10
IamHung1202 <125440531+IamHung1202@users.noreply.github.com>
2023-03-08 14:27:55 +0700

    Update 1.dart

--- 1.dart
+++ 1.dart
@@ -2,6 +2,6 @@ import 'dart:io';
 
 void main() {
   File file = File('hello.txt');
-  file.writeAsStringSync('Vanh\n');
+  file.writeAsStringSync('Hung\n');
   print('Đã thêm Vanh vào file hello.txt');
 }
